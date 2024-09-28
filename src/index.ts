6334e05070bbb13432199d73c18afb5bbae4ce29
weyilham <dilhammaulanaa@gmail.com>
2024-09-28 14:46:42 +0700

    setup prettier dan eslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("oke");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use("/health", (req: Request, res: Response) => {
+  res.status(200).send({
+    data: "hello world",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
